eea37a09bb7b996dc09dfb1df1af56076d0e42ce
John Dunn <alch3myst@gmail.com>
2022-09-10 13:47:47 -0500

    create food model

---
+++ migrations/20220910181746-create-food.js
@@ -0,0 +1,46 @@
+'use strict'
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('food', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      calories: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      protein: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      fats: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      carbohydrates: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('food')
+  }
+}
---
+++ models/food.js
@@ -0,0 +1,29 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Food extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Food.init(
+    {
+      name: DataTypes.STRING,
+      type: DataTypes.STRING,
+      calories: DataTypes.INTEGER,
+      protein: DataTypes.INTEGER,
+      fats: DataTypes.INTEGER,
+      carbohydrates: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Food'
+    }
+  )
+  return Food
+}
