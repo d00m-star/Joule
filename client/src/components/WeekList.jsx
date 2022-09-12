9d0c54d52e0fa6e49976674d45f76e5e0ea0fe66
John Dunn <alch3myst@gmail.com>
2022-09-12 15:30:36 -0500

    css

--- client/src/components/WeekList.jsx
+++ client/src/components/WeekList.jsx
@@ -1,18 +1,11 @@
 const WeekList = ({days}) => {
   return (
     <div className="day-list-container">
+      <p>You only this day to lose weight.</p>
     <div className="day-list">
       {days.map((day) => (
         <div className='day-items' key={day.id}>
-          <h3>{day.name}</h3>
-          <div className="day-dropdown">
-          <ul>
-            <li>Calories: {day.calories}</li>
-            <li>Protein: {day.protein}</li>
-            <li>Fats: {day.fats}</li>
-            <li>Carbohydrates: {day.carbohydrates}</li>
-          </ul>
-          </div>
+          <h3>{day.name} - {day.calories}</h3>
           </div>
       ))}
     </div>
