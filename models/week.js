beb5d3e73bc760e1febb9022b64b15b8ed37e90a
John Dunn <alch3myst@gmail.com>
2022-09-13 08:04:29 -0500

    add tablename to weeks

--- models/week.js
+++ models/week.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Week extends Model {
     /**
@@ -13,12 +11,16 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Week.init({
-    name: DataTypes.STRING,
-    dayId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Week',
-  });
-  return Week;
-};
+  Week.init(
+    {
+      name: DataTypes.STRING,
+      dayId: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Week',
+      tableName: 'weeks'
+    }
+  )
+  return Week
+}
